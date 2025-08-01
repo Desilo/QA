7c8f640b3ed0e308d920e2d5926ea91eb9850d1b
woojae-yang-QA <woojae.yang@desilo.ai>
2025-08-01 16:46:01 +0900

    Update driver_setup.py
    change250801 TRY6

--- driver_setup.py
+++ driver_setup.py
@@ -36,9 +36,9 @@ def create_driver():
     
     options.add_argument("start-maximized")
     options.add_argument("enable-automation")
-    #options.add_argument("--disable-infobars")
+    options.add_argument("--disable-infobars")
     #options.add_argument("--disable-dev-shm-usage")
-    ## test 250801 TRY5
+    ## test 250801 TRY6
 
 
     # 크롬드라이버 자동 업데이트
