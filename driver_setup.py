9d0a5773a055b7e9cb0abcea2b4ef3c3a9efe663
woojae-yang-QA <woojae.yang@desilo.ai>
2025-08-01 14:52:45 +0900

    Update driver_setup.py
    change250801 TRY4

--- driver_setup.py
+++ driver_setup.py
@@ -38,7 +38,7 @@ def create_driver():
     options.add_argument("enable-automation")
     #options.add_argument("--disable-infobars")
     #options.add_argument("--disable-dev-shm-usage")
-    ## test 250801 TRY3
+    ## test 250801 TRY4
 
 
     # 크롬드라이버 자동 업데이트
