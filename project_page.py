1681dbb9464800b5ff02548463a8820df49decf9
woojae-yang-QA <woojae.yang@desilo.ai>
2025-08-27 10:06:35 +0900

    age_btn
    age_btn

--- project_page.py
+++ project_page.py
@@ -639,8 +639,8 @@ class Project_Page:
         time.sleep(2)
         
         self.driver.find_element(By.XPATH, '/html/body/div/div[1]/div[3]/div[2]/div/div/div[2]/div[2]/div[2]/div/div[2]/div[2]/div[2]/div/div[2]/div/div[1]/div[2]/div/div').click()
-        height_btn01 = self.wait.until(EC.presence_of_element_located((By.XPATH, '//div[contains(text(), "AGE")]')))
-        height_btn01.click()
+        age_btn = self.wait.until(EC.presence_of_element_located((By.XPATH, '//div[contains(text(), "AGE")]')))
+        age_btn.click()
         time.sleep(2)
         
         independent_ok_btn = self.driver.find_element(By.XPATH, '/html/body/div/div[1]/div[3]/div[2]/div/div/div[2]/div[2]/div[2]/div/div[2]/div[2]/div[2]/div/div[3]/div/div[2]/button')
