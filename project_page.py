97ae667c21ba2fc81445821dc2b3aebbecf944de
woojae-yang-QA <woojae.yang@desilo.ai>
2025-08-27 11:30:42 +0900

    strata
    strata - DTH
    250827

--- project_page.py
+++ project_page.py
@@ -572,7 +572,7 @@ class Project_Page:
         time.sleep(2)
         
         self.driver.find_element(By.XPATH, '/html/body/div/div[1]/div[3]/div[2]/div/div/div[2]/div[2]/div[2]/div/div[2]/div[2]/div[1]/div[2]/div[2]/div/div[2]/div/div[1]/div[2]/div/div').click()
-        strata_elem = self.wait.until(EC.presence_of_element_located((By.XPATH, '//div[contains(text(), "STATIN")]')))
+        strata_elem = self.wait.until(EC.presence_of_element_located((By.XPATH, '//div[contains(text(), "DTH")]')))
         strata_elem.click()
         time.sleep(2)  
         
