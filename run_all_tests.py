b553e394945c47cbd420335a1f21b45c39b482e9
woojae-yang-QA <woojae.yang@desilo.ai>
2025-08-28 14:08:24 +0900

    Update run_all_tests.py

--- run_all_tests.py
+++ run_all_tests.py
@@ -896,7 +896,7 @@ def scene_12(driver, wait, provider, admin, project):
         print('  Data-registry Start -----')
         time.sleep(2)
         provider.move_to_data_registration()
-        provider.upload_file_main4(da_name_12, today_date)
+        provider.upload_file_main4()
         # 시나리오8 에서는 데이터 등록 자동승인 OFF
         provider.click_approval_box()
         provider.upload_file()
